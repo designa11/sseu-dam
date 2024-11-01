762f62b0bfd5a8d361e4d096e8cdb51780f51ac3
배준표 <oyoungeric@gmail.com>
2024-11-01 09:18:31 +0900

    hide mysql adress

--- Back/DB_connect.py
+++ Back/DB_connect.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
+import ipconfig as ip
 
 
 
-database_url = "mysql+pymysql://Insa5_IOT_final_2:aischool2@project-db-stu3.smhrd.com:3307/Insa5_IOT_final_2"
+database_url = f"mysql+pymysql://Insa5_IOT_final_2:{ip.db_pw}@project-db-stu3.smhrd.com:3307/Insa5_IOT_final_2"
 # mysql+pymysql://<user>:<password>@<host>:<port>/<database>
 engine = create_engine(database_url)
--- Back/ipconfig.py
+++ Back/ipconfig.py
@@ -1,1 +1,2 @@
 client_ip = "http://192.168.20.61:5173"
+db_pw = "aischool2"
